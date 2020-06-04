c3c654d8846b73ca70fb08b95f35eac786eb67b4
sowrabh meduri <msasowrabh55@gmail.com>
2020-06-04 15:04:10 +0530

    added path to toast msg when image is saved and added image analysis use case

--- app/src/main/java/com/example/camerax/MainActivity.java
+++ app/src/main/java/com/example/camerax/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.camerax;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.icu.lang.UCharacter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -24,6 +33,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
+import androidx.camera.extensions.*;
 
 
 
@@ -36,13 +46,22 @@ public class MainActivity extends AppCompatActivity {
     private PreviewView previewView;
     private Preview preview;
     private ImageCapture imageCapture;
+    private  ImageAnalysis imageAnalysis;
     private Button b;
+    private static final int PICK_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         previewView = findViewById(R.id.viewFinder);
+        Button openGallery=(Button) findViewById(R.id.open_gallery);
+        openGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opengallery(view);
+            }
+        });
 
 
         if(allPermissionsGranted()){
@@ -56,6 +75,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "request permission", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void opengallery(View view) {
+        Intent gallery=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+
+            Uri imageUri = data.getData();
+            ImageView imageView=(ImageView)findViewById(R.id.gallery_image);
+            imageView.setImageURI(imageUri);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
@@ -86,14 +121,15 @@ public class MainActivity extends AppCompatActivity {
                 b.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(MainActivity.this,"click",Toast.LENGTH_SHORT).show();
-                        File f = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
+                        File f = new File(Environment.getExternalStorageDirectory() + "/" +"Pictures/"+ System.currentTimeMillis() + ".png");
                         ImageCapture.OutputFileOptions outputFileOptions =
                                 new ImageCapture.OutputFileOptions.Builder(f).build();
+
                         imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(MainActivity.this), new ImageCapture.OnImageSavedCallback() {
                             @Override
                             public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                                Toast.makeText(MainActivity.this,"saved",Toast.LENGTH_SHORT).show();
+                                String msg = "Pic captured at " + f.getAbsolutePath();
+                                Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
                             }
 
                             @Override
@@ -106,10 +142,21 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+                imageAnalysis=new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280,720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(@NonNull ImageProxy image) {
+                        Toast.makeText(MainActivity.this,"Analysing",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
 
 
-                Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,imageCapture, preview);
+
+                Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,imageAnalysis,imageCapture, preview);
                 preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
 
             } catch (ExecutionException | InterruptedException e) {
